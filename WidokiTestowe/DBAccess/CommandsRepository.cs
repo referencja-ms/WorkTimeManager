6dcaf20ddf6b12a7b159411aec37f3ba786bfaea
Szymon <lipieczsp6@gmail.com>
2019-07-02 20:44:45 +0200

    hejka naklejka

--- WidokiTestowe/DBAccess/CommandsRepository.cs
+++ WidokiTestowe/DBAccess/CommandsRepository.cs
@@ -9,24 +9,49 @@ namespace WorkTimeManager.DBAccess
 {
     public static class CommandsRepository
     {
+
+        static MySqlConnection conn = DBConnection.Instance.Connection;
+
+        //logowanie użytkownika
+        public static string GET_LOGIN_WITH_PASSWORD(string login, string password) 
         static MySqlConnection conn = DBConnection.Instance.Connection;
         public static MySqlCommand GET_LOGIN_WITH_PASSWORD(string login, string password)
         {
-            MySqlCommand command = new MySqlCommand();
-            command.CommandType = System.Data.CommandType.Text;
-            command.CommandText = "SELECT login FROM users WHERE login=@UserLogin AND password=@UserPassword";
-            command.Parameters.Add(new MySqlParameter("@UserLogin", login));
-            command.Parameters.Add(new MySqlParameter("@UserPassword", password));
-            return command;
-        }
-        //Pobranie wszystkich projektów zalogowanego użytkownika (id, name)
-        public static MySqlCommand GET_ALL_USERS_PROJECTS(string login)
-        {
-            MySqlCommand command = new MySqlCommand();
-            command.CommandType = System.Data.CommandType.Text;
-            command.CommandText = "SELECT id, name FROM projects, workTasks WHERE id = projectID AND user = @parametr1";
-            command.Parameters.Add(new MySqlParameter("@parametr1", login));
-            return command;
+            try
+            {
+                conn.Open();
+                //utworzenie zapytania do bazy o pozycję użytkownika
+                MySqlCommand command = conn.CreateCommand();
+                command.CommandType = System.Data.CommandType.Text;
+                command.CommandText = "SELECT position FROM users WHERE login=@UserLogin AND password=@UserPassword";
+                command.Parameters.Add(new MySqlParameter("@UserLogin", login));
+                command.Parameters.Add(new MySqlParameter("@UserPassword", password));
+
+                //sprawdzenie, czy baza zwróciła cokolwiek
+                object result = command.ExecuteScalar();
+                //jeśli zwróciła
+                if (result != null)
+                {
+                    var reader = command.ExecuteReader();
+                    reader.Read();
+                    Console.WriteLine(reader["position"].ToString());
+                    DBConnection.Instance.LoggedUser = new Models.User(login, reader["position"].ToString());
+                    return reader["position"].ToString();
+                }
+                //jeśli nie zwróciła - brak użytkownika w bazie
+                else
+                    return "no-user";
+
+            }
+            //obsługa błędu połączenia z bazą
+            catch (MySqlException)
+            {
+                return "no-connection";
+            }
+            finally
+            {
+                conn.Close();
+            }
         }
         public static List<string> GetUserProjects(string login) {
             List<Models.Project> projects = new List<Models.Project>();
